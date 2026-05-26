33866b939023bebbacc9f459a3bbd568c96b8d6e
dami12356 <nguyenbaotam.123456@gmail.com>
2026-05-26 14:00:08 +0900

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -32,6 +32,9 @@ function drawCamera() {
         width,
         height
     );
+    // — 응웬 바오 담 (Tamy) 추가
+drawARFilter(width / 2, height / 2, selectedFilter);
+drawFaceStatus(width, height);
 
     // countdown
     if (countdown > 0) {
