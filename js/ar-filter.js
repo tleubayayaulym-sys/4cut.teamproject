833ae812b0b75b0e8a85328850aa55f065701e2c
tleubayayaulym-sys <tleubay.ayaulym@icloud.com>
2026-06-05 01:39:43 +0900

    Update ar-filter.js

--- js/ar-filter.js
+++ js/ar-filter.js
@@ -309,7 +309,13 @@ function veFilterTim(camX, camY) {
     push();
     translate(tx, ty);
     scale(tl * 0.55);
-    fill(mauTimList[t.mau % mauTimList.length], alf); noStroke();
+    if (typeof selectedFormat !== 'undefined' && selectedFormat !== null) {
+  try {
+    fill(255, 255, 255, 0); 
+  } catch(e) {
+    fill(255, 255, 255, 0);
+  }
+}
     beginShape();
     vertex(0, 20); bezierVertex(-28, 0, -18, -25, 0, -12);
     bezierVertex(18, -25, 28, 0, 0, 20);
